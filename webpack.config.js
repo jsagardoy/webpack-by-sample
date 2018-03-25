0c8d48dc3cfe87f494e225bf7b0d6b575d14d3ec
Jehú <jehu.sagardoy@gmail.com>
2018-03-26 00:40:28 +0200

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,82 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require('path');
+
+var basePath = __dirname;
 
 module.exports = {
-  entry: ['./students.js'],
-  output: {
-    filename: 'bundle.js',
+  context: path.join(basePath, 'src'),
+  entry: {
+    app: './students.js',
+    appStyles: [
+      './mystyles.scss',
+    ],
+    vendor: [
+      'jquery',
+    ],
+    vendorStyles: [
+      '../node_modules/bootstrap/dist/css/bootstrap.css', 
+    ]
+  },
+  output: {        
+    filename: './js/[name].[chunkhash].js',    
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/,                
         exclude: /node_modules/,
         loader: 'babel-loader',
+      },
+      {  
+        test: /\.css$/, 
+        include: /node_modules/,               
+        use: [
+              MiniCssExtractPlugin.loader, 
+              "css-loader"
+        ]
+      },  
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'}
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit:5000,
+            name: './img/[hash].[name].[ext]'
+          }
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
       }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html', // destination file ./dist
-      template: 'index.html', // name of origina html file
-      hash: true,
+      template: 'index.html', // name of origina html file   
+      chunksSortMode: 'manual',
+      chunks: ['vendor', 'vendorStyles', 'appStyles', 'app']
     }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].[chunkhash].css",      
+      chunkFilename: "[id].css"
+    })    
   ]
 }
